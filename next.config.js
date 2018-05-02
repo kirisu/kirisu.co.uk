1496b5b46a3f0702b00dc8a5ee19f53de7f5244a
Christopher Lunt <github@kirisu.co.uk>
2018-05-02 19:39:11 +0100

    Try removing asset prefix.

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 var webpack = require('webpack');
 const withSass = require('@zeit/next-sass');
 
-const debug = process.env.NODE_ENV !== 'production';
-
 module.exports = withSass({
   exportPathMap: function() {
     return {
       '/': { page: '/' }
     };
-  },
-  assetPrefix: !debug ? 'https://kirisu.github.io/kirisu.co.uk/' : ''
+  }
 });
