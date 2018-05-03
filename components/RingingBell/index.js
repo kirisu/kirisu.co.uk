2b3489ad632b5d6fa13d489c7ddc43c4e29a2ec4
Christopher Lunt <github@kirisu.co.uk>
2018-05-03 21:01:27 +0100

    Add contact button.

---
+++ components/RingingBell/index.js
@@ -0,0 +1,35 @@
+import { PureComponent } from 'react';
+
+export default class RingingBell extends PureComponent {
+  state = {
+    angles: [0, 315, 0, 45]
+  };
+
+  componentWillMount() {
+    this.timer = setTimeout(() => {
+      this.setState({
+        angles: [
+          ...this.state.angles.slice(1, this.state.angles.length),
+          this.state.angles[0]
+        ]
+      });
+      this.componentWillMount();
+    }, Math.floor(Date.now() / 1000) * 1000 + 1000 - Date.now());
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timer);
+  }
+
+  render() {
+    return (
+      <span className="icon">
+        <i
+          className={`mdi mdi-bell-ring-outline mdi-rotate-${
+            this.state.angles[0]
+          }`}
+        />
+      </span>
+    );
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,21 @@
-import React from 'react';
-
 import Application from '../layouts/application';
+import RingingBell from '../components/RingingBell';
 
 export default () => (
   <Application title="@kirisu">
     <section className="hero is-fullheight is-light">
       <div className="hero-body">
         <div className="container has-text-centered">
-          <h3 className="title is-3">
-            <a href="https://github.com/kirisu">@kirisu</a>
-          </h3>
+          <h3 className="title is-3">@kirisu</h3>
           <h4 className="subtitle is-4 has-text-grey-light">
             Christopher Lunt is a Software Engineer based in Glasgow, Scotland.
           </h4>
+          <a
+            href="mailto:contact@kirisu.co.uk"
+            className="button is-medium is-primary is-outlined is-rounded">
+            <RingingBell />
+            <span>Contact Me</span>
+          </a>
         </div>
       </div>
     </section>
