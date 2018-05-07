037a06d49b5fd73401f6c00ea8e0543dc1ac281f
Christopher Lunt <kirisu@users.noreply.github.com>
2018-05-07 17:09:21 +0100

    Use travis to create .nojekyll and CNAME files

--- bin/writeCNAME.js
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env node
-const path = require('path');
-const fs = require('fs');
-
-const DESTINATION = path.join(__dirname, '..', 'out', 'CNAME');
-
-fs.writeFileSync(DESTINATION, 'www.kirisu.co.uk');
-
-console.log(`Wrote CNAME file to ${DESTINATION}`);
