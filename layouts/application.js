047f425106ebb5059c23b865467c029af14a3f08
Christopher Lunt <kirisu@users.noreply.github.com>
2018-05-10 15:53:39 +0100

    Move css and meta tags to document level rather than layout.

--- layouts/application.js
+++ layouts/application.js
@@ -7,12 +7,6 @@ export default ({ children, title = '' }) => (
   <>
     <Head>
       <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link
-        rel="stylesheet"
-        href="//cdn.materialdesignicons.com/2.3.54/css/materialdesignicons.min.css"
-      />
     </Head>
     {children}
     <div id="root-modal" />
--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,23 @@
 // ./pages/_document.js
 import Document, { Head, Main, NextScript } from 'next/document';
 
+const FAVICON_URL = `${SITE_ROOT}/static/assets/favicon.ico`;
+const APP_CSS_URL = `${SITE_ROOT}/_next/static/style.css`;
+const MDI_CSS_URL =
+  '//cdn.materialdesignicons.com/2.3.54/css/materialdesignicons.min.css';
+
 export default class App extends Document {
   render() {
     return (
       <html>
         <Head>
-          <link rel="stylesheet" href={`${SITE_ROOT}/_next/static/style.css`} />
-          <link
-            rel="icon"
-            href={`${SITE_ROOT}/static/assets/favicon.ico`}
-            type="image/x-icon"
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
           />
+          <link rel="icon" href={FAVICON_URL} type="image/x-icon" />
+          <link rel="stylesheet" href={APP_CSS_URL} />
+          <link rel="stylesheet" href={MDI_CSS_URL} />
         </Head>
         <body>
           <Main />
